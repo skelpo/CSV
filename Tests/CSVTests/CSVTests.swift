cf98d15766b320ef6ced1e1096f48858dc4119e7
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-15 09:18:34 -0600

    Wipped CSVTest.testExample methods

--- Tests/CSVTests/CSVTests.swift
+++ Tests/CSVTests/CSVTests.swift
@@ -3,13 +3,9 @@ import XCTest
 
 class CSVTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CSV().text, "Hello, World!")
+
     }
-
-
+    
     static var allTests = [
         ("testExample", testExample),
     ]
