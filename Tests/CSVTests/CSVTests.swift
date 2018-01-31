413618e814ad9e15e3fb8595bd806edbc81ff9d5
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-31 14:43:42 -0600

    Added speed test

--- Tests/CSVTests/CSVTests.swift
+++ Tests/CSVTests/CSVTests.swift
@@ -3,7 +3,19 @@ import XCTest
 
 class CSVTests: XCTestCase {
     func testExample() {
-
+        do {
+            let url = URL(string: "file:/Users/calebkleveter/Development/Fielding.csv")!
+            let data = try Data(contentsOf: url)
+            
+            print("Start")
+            
+            measure {
+                let _: [CSV.Column] = CSV.parse(data)
+            }
+            
+        } catch let error {
+            XCTFail("\(error)")
+        }
     }
     
     static var allTests = [
