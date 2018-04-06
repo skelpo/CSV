a435fa851930381df6fef96e92923f291af6aa57
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-06 10:37:36 -0500

    Updated Core version to 3.0.2

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "CSV", targets: ["CSV"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/core.git", from: "3.0.0-rc")
+        .package(url: "https://github.com/vapor/core.git", from: "3.0.0")
     ],
     targets: [
         .target(name: "CSV", dependencies: ["Bits"]),
