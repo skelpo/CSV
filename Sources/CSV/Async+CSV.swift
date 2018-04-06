4c9390afea456745afbe08e7e703b43751e345e8
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-06 15:04:25 -0500

    Made Future<Data>.parseCSV() methods public

--- Sources/CSV/Async+CSV.swift
+++ Sources/CSV/Async+CSV.swift
@@ -2,20 +2,19 @@ import Foundation
 import Async
 
 extension Future where T == Data {
-    
-    func parseCSV() -> Future<[CSV.Column]> {
+    public func parseCSV() -> Future<[CSV.Column]> {
         return self.map(to: [CSV.Column].self) { (data) in
             return CSV.parse(data)
         }
     }
     
-    func parseCSV() -> Future<[String: CSV.Column]> {
+    public func parseCSV() -> Future<[String: CSV.Column]> {
         return self.map(to: [String: CSV.Column].self) { (data) in
             return CSV.parse(data)
         }
     }
     
-    func parseCSV() -> Future<[String: [String?]]> {
+    public func parseCSV() -> Future<[String: [String?]]> {
         return self.map(to: [String: [String?]].self) { (data) in
             return CSV.parse(data)
         }
