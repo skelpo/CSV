efda57d3ad4b5e9bc496c5dbfc306c2ce674a53a
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-06 15:10:43 -0500

    Removed CSV reducing from CSV.parse methods

--- Sources/CSV/CSV.swift
+++ Sources/CSV/CSV.swift
@@ -8,18 +8,71 @@ public struct CSV {
     }
     
     public static func parse(_ data: Data) -> [String: [String?]] {
-        return parse(data).reduce(into: [:]) { (columns, column) in
-            columns[column.header] = column.fields
+        let rows = data.split(separator: .newLine, omittingEmptySubsequences: false)
+        var cells = rows.map({ $0.split(separator: .comma, omittingEmptySubsequences: false) })
+        let rowLength = cells[0].count - 1
+        
+        for count in 1...cells.count - 1 {
+            if cells[cells.count - count].count < rowLength {
+                _ = cells.removeLast()
+            } else {
+                break
+            }
         }
+        
+        var columns: [String: [String?]] = [:]
+        (0...rowLength).forEach { (cellIndex) in
+            var column = cells.map({ (row) -> String? in
+                return String(data: row[cellIndex], encoding: .utf8)
+            })
+            let title = column.removeFirst()!
+            columns[title] = column
+        }
+        return columns
     }
     
     public static func parse(_ data: Data) -> [String: Column] {
-        return parse(data).reduce(into: [:]) { (columns, column) in
-            columns[column.header] = column
+        let rows = data.split(separator: .newLine, omittingEmptySubsequences: false)
+        var cells = rows.map({ $0.split(separator: .comma, omittingEmptySubsequences: false) })
+        let rowLength = cells[0].count - 1
+        
+        for count in 1...cells.count - 1 {
+            if cells[cells.count - count].count < rowLength {
+                _ = cells.removeLast()
+            } else {
+                break
+            }
         }
+        
+        var columns: [String: Column] = [:]
+        (0...rowLength).forEach { (cellIndex) in
+            var column = cells.map({ (row) -> String? in
+                return String(data: row[cellIndex], encoding: .utf8)
+            })
+            let title = column.removeFirst()!
+            columns[title] = CSV.Column(header: title, fields: column)
+        }
+        return columns
     }
     
     public static func parse(_ data: Data) -> [Column] {
-        return CSV.standardParse(data)
+        let rows = data.split(separator: .newLine, omittingEmptySubsequences: false)
+        var cells = rows.map({ $0.split(separator: .comma, omittingEmptySubsequences: false) })
+        let rowLength = cells[0].count - 1
+        
+        for count in 1...cells.count - 1 {
+            if cells[cells.count - count].count < rowLength {
+                _ = cells.removeLast()
+            } else {
+                break
+            }
+        }
+        
+        return (0...rowLength).map { (cellIndex) -> CSV.Column in
+            var column = cells.map({ (row) -> String? in
+                return String(data: row[cellIndex], encoding: .utf8)
+            })
+            return CSV.Column(header: column.removeFirst()!, fields: column)
+        }
     }
 }
