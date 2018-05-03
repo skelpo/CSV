5e96cbced8aa5a6e439728c8d81fe3eff6dbca84
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-03 14:25:47 -0500

    Added CSV row and cell storage to _CSVDecoder

--- Sources/CSV/Coder/Decoder/_CSVDecoder.swift
+++ Sources/CSV/Coder/Decoder/_CSVDecoder.swift
@@ -5,12 +5,33 @@ public typealias CodingPath = [CodingKey]
 final class _CSVDecoder: Decoder {
     let codingPath: [CodingKey]
     let userInfo: [CodingUserInfoKey : Any]
-    let csv: [String: [String?]]
+    
+    let csv: [String: [String?]]?
+    let row: [String: String?]?
+    let cell: String?
     
     init(csv: [String: [String?]], path: CodingPath = [], info: [CodingUserInfoKey : Any] = [:]) {
         self.codingPath = path
         self.userInfo = info
         self.csv = csv
+        self.row = nil
+        self.cell = nil
+    }
+
+    init(row: [String: String?], path: CodingPath = [], info: [CodingUserInfoKey : Any] = [:]) {
+        self.codingPath = path
+        self.userInfo = info
+        self.csv = nil
+        self.row = row
+        self.cell = nil
+    }
+    
+    init(cell: String?, path: CodingPath = [], info: [CodingUserInfoKey : Any] = [:]) {
+        self.codingPath = path
+        self.userInfo = info
+        self.csv = nil
+        self.row = nil
+        self.cell = cell
     }
     
     func container<Key>(keyedBy type: Key.Type) throws -> KeyedDecodingContainer<Key> where Key : CodingKey {
