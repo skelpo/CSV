f58bfbc8059cff60a36082e3a84957a2a79e25d7
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-04 07:29:31 -0500

    Created static CSVCoder.encode(_:) method

--- Sources/CSV/Coder/CSVCoder.swift
+++ Sources/CSV/Coder/CSVCoder.swift
@@ -4,4 +4,8 @@ public final class CSVCoder {
     public static func decode<T>(_ data: Data, to type: T.Type = T.self)throws -> [T] where T: Decodable {
         return try _CSVDecoder.decode(T.self, from: data)
     }
+    
+    public static func encode<T>(_ objects: [T])throws -> Data where T: Encodable {
+        return try _CSVEncoder.encode(objects)
+    }
 }
