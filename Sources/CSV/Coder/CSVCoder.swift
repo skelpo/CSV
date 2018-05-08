a8e61a3591088ff1949efddf6d2d4675b1ad25d6
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-08 15:29:14 -0500

    Added stringDecoding property to _CSVDecoder class for encoding type of decoded strings

--- Sources/CSV/Coder/CSVCoder.swift
+++ Sources/CSV/Coder/CSVCoder.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 public final class CSVCoder {
-    public static func decode<T>(_ data: Data, to type: T.Type = T.self)throws -> [T] where T: Decodable {
-        return try _CSVDecoder.decode(T.self, from: data)
+    public static func decode<T>(_ data: Data, to type: T.Type = T.self, stringDecoding: String.Encoding = .utf8)throws -> [T] where T: Decodable {
+        return try _CSVDecoder.decode(T.self, from: data, stringDecoding: stringDecoding)
     }
     
     public static func encode<T>(_ objects: [T], boolEncoding: BoolEncodingStrategy = .toString, stringEncoding: String.Encoding = .utf8)throws -> Data where T: Encodable {
--- Sources/CSV/Coder/Decoder/_CSVDecoder.swift
+++ Sources/CSV/Coder/Decoder/_CSVDecoder.swift
@@ -5,12 +5,13 @@ public typealias CodingPath = [CodingKey]
 final class _CSVDecoder: Decoder {
     let codingPath: [CodingKey]
     let userInfo: [CodingUserInfoKey : Any]
+    let stringDecoding: String.Encoding
     
     let csv: [String: [Data?]]?
     let row: [String: Data]?
     let cell: Data?
     
-    init(csv: [String: [Data?]], path: CodingPath = [], info: [CodingUserInfoKey : Any] = [:]) {
+    init(csv: [String: [Data?]], path: CodingPath = [], info: [CodingUserInfoKey : Any] = [:], stringDecoding: String.Encoding) {
         self.codingPath = path
         self.userInfo = info
         self.csv = csv
@@ -18,7 +19,7 @@ final class _CSVDecoder: Decoder {
         self.cell = nil
     }
 
-    init(row: [String: Data], path: CodingPath = [], info: [CodingUserInfoKey : Any] = [:]) {
+    init(row: [String: Data], path: CodingPath = [], info: [CodingUserInfoKey : Any] = [:], stringDecoding: String.Encoding) {
         self.codingPath = path
         self.userInfo = info
         self.csv = nil
@@ -26,7 +27,7 @@ final class _CSVDecoder: Decoder {
         self.cell = nil
     }
     
-    init(cell: Data?, path: CodingPath = [], info: [CodingUserInfoKey : Any] = [:]) {
+    init(cell: Data?, path: CodingPath = [], info: [CodingUserInfoKey : Any] = [:], stringDecoding: String.Encoding) {
         self.codingPath = path
         self.userInfo = info
         self.csv = nil
@@ -72,13 +73,13 @@ final class _CSVDecoder: Decoder {
         return _CSVSingleValueDecoder(value: cell, path: self.codingPath)
     }
     
-    static func decode<T>(_ type: T.Type, from data: Data)throws -> [T] where T: Decodable {
-        let csv: [String: [Data?]] = try _CSVDecoder.organize(data)
+    static func decode<T>(_ type: T.Type, from data: Data, stringDecoding: String.Encoding)throws -> [T] where T: Decodable {
+        let csv: [String: [Data?]] = try _CSVDecoder.organize(data, stringDecoding: stringDecoding)
         let decoder = _CSVDecoder(csv: csv)
         return try Array<T>(from: decoder)
     }
     
-    static func organize(_ data: Data)throws -> [String: [Data?]] {
+    static func organize(_ data: Data, stringDecoding: String.Encoding)throws -> [String: [Data?]] {
         let rows = data.split(separator: .newLine, omittingEmptySubsequences: false)
         var cells = rows.map({ $0.split(separator: .comma, omittingEmptySubsequences: false) })
         let rowLength = cells[0].count - 1
@@ -96,8 +97,8 @@ final class _CSVDecoder: Decoder {
             var column = cells.map({ (row) -> Data? in
                 return row[cellIndex].count > 0 ? row[cellIndex] : nil
             })
-            guard let title = String(data: column.removeFirst()!, encoding: .utf8) else {
-                throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: [], debugDescription: "Found colunm title wuth UTF-8 incompatible character"))
+            guard let title = String(data: column.removeFirst()!, encoding: stringDecoding) else {
+                throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: [], debugDescription: "Found colunm title with \(stringDecoding) incompatible character"))
             }
             columns[title] = column
         }
