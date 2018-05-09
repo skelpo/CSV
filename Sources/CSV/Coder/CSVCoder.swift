5128bb65b9b166fe11cc1b24682d075daa913301
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-09 08:56:36 -0500

    Set default decoder string encoding type to UTF-32

--- Sources/CSV/Coder/CSVCoder.swift
+++ Sources/CSV/Coder/CSVCoder.swift
@@ -5,7 +5,7 @@ public final class CSVCoder {
         return try _CSVDecoder.decode(T.self, from: data, stringDecoding: stringDecoding)
     }
     
-    public static func encode<T>(_ objects: [T], boolEncoding: BoolEncodingStrategy = .toString, stringEncoding: String.Encoding = .utf8)throws -> Data where T: Encodable {
+    public static func encode<T>(_ objects: [T], boolEncoding: BoolEncodingStrategy = .toString, stringEncoding: String.Encoding = .utf32)throws -> Data where T: Encodable {
         return try _CSVEncoder.encode(objects, boolEncoding: boolEncoding, stringEncoding: stringEncoding)
     }
 }
