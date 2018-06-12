987f5647b96ba62ea2b20ec2e9f287290e7462de
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-06-12 13:08:52 -0500

    Redocumented average decoding speed

--- Tests/CSVTests/CSVTests.swift
+++ Tests/CSVTests/CSVTests.swift
@@ -29,7 +29,7 @@ class CSVTests: XCTestCase {
         let url = URL(string: "file:/Users/calebkleveter/Development/developer_survey_2018.csv")!
         let data = try Data(contentsOf: url)
         
-        // 77.769
+        // 58.806
         measure {
             do {
                 _ = try CSVCoder.decode(data, to: Response.self)
