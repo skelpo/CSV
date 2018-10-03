deb14dc76f9a1eac692577b1f121ec1fd6418628
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-03 13:16:40 -0500

    Updated ouddated RC APIs to 3.0 version

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
         .library(name: "CSV", targets: ["CSV"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/core.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/crypto.git", from: "3.1.2")
+        .package(url: "https://github.com/vapor/core.git", from: "3.4.3"),
+        .package(url: "https://github.com/vapor/crypto.git", from: "3.3.0")
     ],
     targets: [
         .target(name: "CSV", dependencies: ["Bits", "Debugging", "Async", "Core"]),
