2801681d5cd7140a31072f76b8cab350a894046f
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-04-19 08:55:04 -0500

    Created StressTests test suite with .testMeasureAsyncParsing test case

---
+++ Tests/CSVTests/StressTests.swift
@@ -0,0 +1,23 @@
+import XCTest
+import CSV
+
+final class StressTests: XCTestCase {
+    let data: Data = {
+        let string: String
+        if let envVar = ProcessInfo.processInfo.environment["CSV_STRESS_TEST_DATA"] { string = "file:" + envVar }
+        else { string = "https://drive.google.com/uc?export=download&id=1_9On2-nsBQIw3JiY43sWbrF8EjrqrR4U" }
+        print(string)
+        let url = URL(string: string)!
+        return try! Data(contentsOf: url)
+    }()
+
+    func testMeasureAsyncParsing() {
+        var parser = Parser(onHeader: { _ in return }, onCell: { _, _ in return })
+        let csv = Array(data)
+
+        // Baseline: 4.630
+        measure {
+            _ = parser.parse(csv)
+        }
+    }
+}
