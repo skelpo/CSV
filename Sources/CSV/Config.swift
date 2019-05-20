4b138390182fcb3e631e26b23f35ea35f8d85519
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-20 08:41:24 -0500

    Documented Config properties

--- Sources/CSV/Config.swift
+++ Sources/CSV/Config.swift
@@ -1,20 +1,26 @@
-/// Wraps the Configuration options for Parse/Encode/Decode
+// Wraps the Configuration options for Parse/Encode/Decode
 
 
 /// The `Config` struct allows for configuring `Parser` and `Serializer`
 /// to allow for separators other than comma or string delimiters
 /// like quotation marks
 public struct Config {
+
+    /// The character that separates one cell from another.
     public let cellSeparator: UInt8
+
+    /// The character that is used to denote the start and end of a cell's contents.
     public let cellDelimiter: UInt8?
-    
+
+    /// The deault `Config` instance that uses commas for cell separators and double quotes
+    /// for cell delimiters.
     public static let `default`: Config = Config(cellSeparator: 44, cellDelimiter: 34)
     
     /// Creates a new `Config` instance
     ///
     /// - Parameters:
-    ///   - cellSeparator: the cell separator (44 for "," by default)
-    ///   - cellDelimiter: the string delimiter (nil by default, 34 would be for question marks)
+    ///   - cellSeparator: The character that separates one cell from another.
+    ///   - cellDelimiter: The character that is used to denote the start and end of a cell's contents.
     public init(cellSeparator: UInt8, cellDelimiter: UInt8?) {
         self.cellSeparator = cellSeparator
         self.cellDelimiter = cellDelimiter
